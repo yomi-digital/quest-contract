06dc93b5f9552a240e50d1bc276b621d4052df78
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-05-23 05:15:05 +0200

    fix uri

--- scripts/fixuris.js
+++ scripts/fixuris.js
@@ -8,7 +8,7 @@ async function main() {
     let wallet = new ethers.Wallet(configs.owner_key).connect(provider)
     const vault = new ethers.Wallet.fromMnemonic(configs.owner_mnemonic, "m/44'/60'/0'/0/2").connect(provider)
     const contract = new ethers.Contract(configs.contract_address, ABI.abi, wallet)
-    console.log('Changing vault to ' + vault.address)
+    console.log('Changing uri to ' + vault.address)
     const result = await contract.fixURIs(0, "https://h5mr3z5vsk.execute-api.us-east-1.amazonaws.com/dev/nfts/")
     console.log(result)
 }
